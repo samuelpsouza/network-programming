3f4495d9a807af7760936d9149112abe07f9d924
Samuel Souza <samuelsouza.contact@gmail.com>
2021-04-18 17:12:21 -0300

    creating udp socket

--- udp-socket-server/src/udp-socket-server.c
+++ udp-socket-server/src/udp-socket-server.c
@@ -14,6 +14,27 @@
 #include <sys/socket.h>
 #include <netdb.h>
 
+#define MAXBUF 1024
+
 int main(int argc, char **argv) {
+	if (2 > argc) {
+		fprintf(stderr, "Usage: %s <port>\n", argv[0]);
+		exit(EXIT_FAILURE);
+	}
+
+	int port = atoi(argv[1]);
+	struct sockaddr_in udp_server;
+	struct sockaddr_in udp_client;
+
+	int udp_socket = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+	if (udp_socket == -1) {
+		fprintf(stderr, "Could not create a socket!\n");
+		exit(EXIT_FAILURE);
+	} else {
+		fprintf(stdout, "Socket created.\n");
+	}
+
+
+	close(udp_socket);
 	return EXIT_SUCCESS;
 }
