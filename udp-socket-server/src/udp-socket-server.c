0d2ae5544600f89bf6f556456d1c36d55dec659a
Samuel Souza <samuelsouza.contact@gmail.com>
2021-04-18 16:50:50 -0300

    creating udp server

---
+++ udp-socket-server/src/udp-socket-server.c
@@ -0,0 +1,19 @@
+/*
+ ============================================================================
+ Name        : udp-socket-server.c
+ Author      : Samuel
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+
+int main(int argc, char **argv) {
+	return EXIT_SUCCESS;
+}
