2baee093d279511ca531debb7927b1ec3a56d292
Samuel Souza <samuelsouza.contact@gmail.com>
2021-05-14 22:09:03 -0300

    solving Perteson exercies 32 and 33

---
+++ simplex-talk-client/run.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+for i in 1 2 3 4 5 6 7 8 9 10
+do
+  echo "--------------------------------------------------------" >> log.txt
+  echo "Starting client $i" >> log.txt
+  ./Debug/simplex-talk-client 127.0.0.1 < input/c$i.txt 2&>> log.txt &
+done 
