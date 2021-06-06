be68231a84c4070e7104d88b6f47cc6fad4612db
Samuel Souza <samuelsouza.contact@gmail.com>
2021-06-06 00:19:05 -0300

    mail client

--- kurose-ross/mail/mail.py
+++ kurose-ross/mail/mail.py
@@ -1,12 +1,15 @@
 #!/usr/bin/env python3
  
 import argparse
- 
+import json
+import base64
+from os import path
 import sys
-import socket
+import socket, ssl
 from socket import socket as Socket
  
- 
+DEFAUTL_ENCODE = "utf-8"
+
 def main():
  
     parser = argparse.ArgumentParser()
@@ -22,16 +25,73 @@ def main():
  
     # success
     return 0
- 
+
+def print_reply_message(head, msg):
+    print("{} {}".format(head, msg.decode("utf-8")))
+
+    return 
  
 def send_mail(my_address, mail_server, their_address, message):
  
     # Fill in the code to talk to the mail server here. Read the file
     # read-first.md to find out how to set up a mail server for testing. A log
     # of a telnet session with your mail server may be (very) useful here.
- 
-    raise NotImplementedError
- 
+    
+    server_addr = (mail_server, 465)
+    user = None
+    pwd = None
+
+    filepath = path.abspath(path.join(path.dirname(__file__), 'credentials.json'))
+
+    with open(filepath) as credentials:
+        data = json.load(credentials)
+        user = data['username']
+        pwd = data['password']
+
+    with ssl.wrap_socket(Socket(socket.AF_INET, socket.SOCK_STREAM), ssl_version=ssl.PROTOCOL_SSLv23) as client_socket:
+        client_socket.connect(server_addr)
+        res = client_socket.recv(1024)
+        print_reply_message("Response for connection request:", res)
+
+        client_socket.send("EHLO there\n".encode(DEFAUTL_ENCODE))
+        res = client_socket.recv(1024)
+        print_reply_message("Response for EHLO request:", res)
+
+        client_socket.send("AUTH LOGIN\n".encode(DEFAUTL_ENCODE))
+        res = client_socket.recv(1024)
+        print_reply_message("Response for AUTH LOGIN request:", res)
+
+        client_socket.send(base64.b64encode(user.encode(DEFAUTL_ENCODE)))
+        client_socket.send("\r\n".encode(DEFAUTL_ENCODE))
+        res = client_socket.recv(2048)
+        print_reply_message("Response for user sent:", res)
+
+        client_socket.send(base64.b64encode(pwd.encode(DEFAUTL_ENCODE)))
+        client_socket.send("\r\n".encode(DEFAUTL_ENCODE))
+        res = client_socket.recv(2048)
+        print_reply_message("Response for pwd sent:", res)
+
+        client_socket.send("MAIL FROM:<{}>\n".format(my_address).encode(DEFAUTL_ENCODE))
+        res = client_socket.recv(1024)
+        print_reply_message("Response for MAIL FROM request:", res)
+
+        for addr in their_address:
+            client_socket.send("RCPT TO:<{}>\n".format(addr).encode(DEFAUTL_ENCODE))
+            res = client_socket.recv(1024)
+                
+        client_socket.send("DATA\n".encode(DEFAUTL_ENCODE))
+        res = client_socket.recv(1024)
+        print_reply_message("Response for DATA request:", res)
+
+        client_socket.send("{}\n".format(message).encode(DEFAUTL_ENCODE))
+        res = client_socket.recv(1024)
+        print_reply_message("Response for sending message:", res)
+
+        client_socket.send("QUIT\n".encode(DEFAUTL_ENCODE))
+        res = client_socket.recv(1024)
+        print_reply_message("Response for QUIT request:", res)
+        
+        client_socket.close()
     return
      
  
